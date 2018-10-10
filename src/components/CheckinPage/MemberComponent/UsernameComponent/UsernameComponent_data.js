1cf79240abcbce0c0a628ee1e855ca9661665412
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-10-09 19:04:12 -0500

    added more UI components to Checkin page

---
+++ src/components/CheckinPage/MemberComponent/UsernameComponent/UsernameComponent_data.js
@@ -0,0 +1,30 @@
+const UsernameComponent_data = [
+
+  {
+    name: 'Abe Lincoln',
+    organization: 'The Government',
+    phoneNumber: 5555555555,
+    avatar: 'https://via.placeholder.com/45x45'
+  },
+  {
+    name: 'George Washington',
+    organization: 'The Federation',
+    phoneNumber: 5555555555,
+    avatar: 'https://via.placeholder.com/45x45'
+  },
+  {
+    name: 'Franklin D. Roosevelt',
+    organization: 'The Institution',
+    phoneNumber: 5555555555,
+    avatar: 'https://via.placeholder.com/45x45'
+  },
+  {
+    name: 'Barack Obama',
+    organization: 'United State of Forever',
+    phoneNumber: 5555555555,
+    avatar: 'https://via.placeholder.com/45x45'
+  }
+
+];
+
+export default UsernameComponent_data;
