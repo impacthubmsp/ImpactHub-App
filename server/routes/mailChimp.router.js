466b78e29bf3c71d789f1d609304f76c2e226daf
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-10-15 10:01:15 -0500

    finished addVisitor route

--- server/routes/mailChimp.router.js
+++ server/routes/mailChimp.router.js
@@ -13,28 +13,33 @@ router.get('/getList', (req, res) => {
         console.log(response.data);
         res.send(response.data)
     }).catch((error) => {
-        console.log('error in mail get look here: ', error);
+        console.log('error in /getList: ', error);
     })
 });
 
 
-// This request will retrieve all the members inside of the "interested in membership" list.
+// This request will post the new user to the email list
 // Modify the list id to change for impacthub's api
-router.get('/post', (req, res) => {
+router.post('/addVisitor', (req, res) => {
     let userToAdd = {
-
+        "email_address": "hello@jclarkworks.com",
+        "status": "subscribed",
+        "merge_fields": {
+            "FNAME": "Jakeh",
+            "LNAME": 'Clark',
+            "PHONE": "2146776670",
+        }
     }
-
     axios({
         url: `https://us19.api.mailchimp.com/3.0/lists/4199edf633/members`,
         method: 'POST',
         headers: { Authorization: `user ${process.env.APIKEY}` },
-        body: userToAdd,
+        data: userToAdd,
     }).then((response) => {
         console.log(response.data);
-        res.send(response.data)
+        res.sendStatus(200)
     }).catch((error) => {
-        console.log('error in mail get look here: ', error);
+        console.log('error in /addVisitor: ', error);
     })
 });
 module.exports = router;
