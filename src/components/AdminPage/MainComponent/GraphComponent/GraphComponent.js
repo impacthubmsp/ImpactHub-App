d3d3bfdcfc4e16fc6f1e7620407a2e77a650c678
k-burn <karaburnett93@gmail.com>
2018-10-21 15:29:16 -0500

    formatted data for different visit traffic line graphs

--- src/components/AdminPage/MainComponent/GraphComponent/GraphComponent.js
+++ src/components/AdminPage/MainComponent/GraphComponent/GraphComponent.js
@@ -9,8 +9,15 @@ class GraphComponent extends Component {
     super();
     this.state = {
       graphToShow: 'today',
-      memberVisitByMonth: '',
+      memberVisitToday: [],
+      visitorVisitToday: [],
+      memberVisitThisWeek: [],
+      visitorVisitThisWeek: [],
+      memberVisitThisMonth: [],
+      visitorVisitThisMonth: [],
+      memberVisitByMonth: [65, 59, 80, 81, 56, 55, 40, 50, 45, 89, 87, 30],
       visitorVisitByMonth: [29, 78, 76, 34, 89, 73, 34, 89, 23, 134, 67, 78],
+    
     }
   }
   // Renders graph displayed based on variable selected from dropdown
@@ -21,7 +28,148 @@ class GraphComponent extends Component {
 }
   render() {
     const showGraph=this.state.graphToShow;
+    const dailyData = {
+      labels: ['7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm', '8:00 pm', '9:00pm'],
+      datasets: [
+        {
+          label: 'Members',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: '#b2dfdb',
+          borderColor: '#b2dfdb',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.memberVisitToday
+        },
+        {
+          label: 'Visitors',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: '#ffab91',
+          borderColor: '#ffab91',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.visitorVisitToday
+        }
+      ]
+    };
+    const weeklyData = {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      datasets: [
+        {
+          label: 'Members',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: '#b2dfdb',
+          borderColor: '#b2dfdb',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.memberVisitToday
+        },
+        {
+          label: 'Visitors',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: '#ffab91',
+          borderColor: '#ffab91',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.visitorVisitToday
+        }
+      ]
+    };
     const monthlyData = {
+      labels: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      datasets: [
+        {
+          label: 'Members',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: '#b2dfdb',
+          borderColor: '#b2dfdb',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.memberVisitByMonth
+        },
+        {
+          label: 'Visitors',
+          fill: true,
+          lineTension: 0.1,
+          backgroundColor: '#ffab91',
+          borderColor: '#ffab91',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.state.visitorVisitByMonth
+        }
+      ]
+    };
+    const yearlyData = {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
         {
@@ -43,7 +191,7 @@ class GraphComponent extends Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40, 50, 45, 89, 87, 30]
+          data: this.state.memberVisitByMonth
         },
         {
           label: 'Visitors',
@@ -76,7 +224,7 @@ class GraphComponent extends Component {
           <option value="thisMonth"> This Month </option>
           <option value="thisYear">This Year</option>
         </select>
-        {showGraph==='today' && <Line id="todayLine" data={monthlyData}
+        {showGraph==='today' && <Line id="todayLine" data={dailyData}
           options={{
             title: {
               display: true,
@@ -90,7 +238,7 @@ class GraphComponent extends Component {
               yAxes: [{ stacked: true }]
             }
           }} />}
-        {showGraph=="thisWeek" && <Line id="weekLine" data={monthlyData}
+        {showGraph=="thisWeek" && <Line id="weekLine" data={weeklyData}
           options={{
             title: {
               display: true,
@@ -118,7 +266,7 @@ class GraphComponent extends Component {
               yAxes: [{ stacked: true }]
             }
           }} />}
-        {showGraph==='thisYear' && <Line id="monthLine" data={monthlyData}
+        {showGraph==='thisYear' && <Line id="yearLine" data={yearlyData}
           options={{
             title: {
               display: true,
@@ -138,4 +286,3 @@ class GraphComponent extends Component {
 }
 
 export default GraphComponent;
-
