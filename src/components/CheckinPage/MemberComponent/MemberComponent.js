0d9a73d800d67bc6b1ed96c738d549651357d000
Sam Bahta <smabahta37@gmail.com>
2018-10-25 10:43:58 -0500

    presentation mode

--- src/components/CheckinPage/MemberComponent/MemberComponent.js
+++ src/components/CheckinPage/MemberComponent/MemberComponent.js
@@ -199,11 +199,6 @@ class MemberComponent extends Component {
                 this.setState({
                     checked_in: false
                 })
-            }
-            else {
-                this.setState({
-                    checked_in: true
-                })
                 break;
             }
         }
