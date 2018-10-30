a51e080a492cf010bb496e21f9b5eccc4d1e276d
Sam Bahta <smabahta37@gmail.com>
2018-10-30 14:37:30 -0500

    heroku deploy

---
+++ build/precache-manifest.a4f08c0fd091c60963d3e8ec75570a79.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f4b8cd0d34b06878057",
+    "url": "/static/js/main.2f4b8cd0.chunk.js"
+  },
+  {
+    "revision": "4173ee753e21718ecaf3",
+    "url": "/static/js/1.4173ee75.chunk.js"
+  },
+  {
+    "revision": "2f4b8cd0d34b06878057",
+    "url": "/static/css/main.d19b238b.chunk.css"
+  },
+  {
+    "revision": "67c6f3d3b8ace56ce2bf96ee55ef7eb4",
+    "url": "/index.html"
+  }
+];
